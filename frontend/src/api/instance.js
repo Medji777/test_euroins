eaf8fd88ad373019b0b30c6a9912862d4bf79c35
Medji <17dergachev@gmail.com>
2019-10-06 22:51:02 +0300

    Add comments for axios instance

--- frontend/src/api/instance.js
+++ frontend/src/api/instance.js
@@ -1,13 +1,16 @@
 import * as instance from 'axios';
 
+// настраиваем instance axios
 const axios = instance.create({
     baseURL: 'http://localhost:8000/'
 });
 
+// настраиваем interceptors axios для перехвата token
 axios.interceptors.request.use((config)=>{
 
     let token = localStorage.getItem('token');
 
+    // если token есть, то добавляем его в заголовки запроса
     if(token !== null){
         config.headers.token = token
     }
